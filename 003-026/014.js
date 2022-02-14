be9528ea40aa1439517825f83ee50d5cc53e7220
liberoLab <libero221.lab@gmail.com>
2022-02-14 21:58:19 +0900

    정리

---
+++ 003-026/014.js
@@ -0,0 +1,7 @@
+const string = 'hello'
+const string2 = 'The method then selects elements form the start argument, and up to (but not including) the end argument.'
+
+console.log(`string: ${string.length}`)
+console.log(`string2: ${string2.length}`)
+console.log(`string[0]: ${string[0]}`) //배열이 아니여도 불러올 수 있다
+console.log(`string2[104]: ${string2[104]}`)
