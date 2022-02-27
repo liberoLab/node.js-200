39f13008d91270e6db4205640bd17224b632daea
liberoLab <libero221.lab@gmail.com>
2022-02-28 02:31:09 +0900

    정리

---
+++ Part2(027-092)/035.js
@@ -0,0 +1,14 @@
+const listUser = [
+    { name: 'kyeongrok', age: 31 },
+    { name: 'jihyun', age: 31 },
+    { name: 'minsup', age: 35 },
+];
+
+//forEach로 listUser의 요소를 한개씩 뽑아서 user 함수에 넣어준다.
+listUser.forEach(function (user) {
+    console.log(user);
+});
+console.log('--------------------')
+
+//arrow function
+listUser.forEach(user => console.log(user))
