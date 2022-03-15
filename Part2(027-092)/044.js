d9876eabf5201910f0600581b09024d714b48917
iberoLab <libero221.lab@gmail.com>
2022-03-16 03:03:13 +0900

    정리

---
+++ Part2(027-092)/044.js
@@ -0,0 +1,17 @@
+function printHello() {
+    console.log('Hello');
+}
+
+printHello();
+
+
+/*
+화살표 함수 선언 방법
+function 대신 변수 = () => {} 으로 작성
+*/
+
+const printBye = () => {
+    console.log('bye');
+}
+
+printBye();
