6bccfae7e624188bb71c5165560378143096121e
liberoLab <libero221.lab@gmail.com>
2022-03-18 03:02:54 +0900

    정리

--- Part2(027-092)/056.js
+++ Part2(027-092)/056.js
@@ -2,5 +2,6 @@ const number = 88.7;
 const divide = 1/2;
 
 console.log('type:%s %d', typeof number, number);
+//Math.round 를 사용하면 숫자를 반올림 해서 출력해 준다.
 console.log('round', typeof number, Math.round(number));
 console.log('divide: %d, %d', divide, Math.round(divide));
